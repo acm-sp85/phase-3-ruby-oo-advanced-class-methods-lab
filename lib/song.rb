19c536959c2435d7da149c1ea93b80f9dc391fc0
alex <acontell@gmail.com>
2021-08-05 12:48:15 +0200

    Failing 3 tests

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,35 @@ class Song
   def save
     self.class.all << self
   end
+  def self.create
+    new_song= self.new
+    @@all << new_song
+    return new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    return new_song
+  end
+  def self.create_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    @@all << new_song
+    return new_song
+  end
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  def self.find_or_create_by_name(name)
+    if Song.find_by_name(name) 
+      return Song.find_by_name(name)
+    else
+      return Song.create_by_name(name)
+    end
+  end
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
 
 end
